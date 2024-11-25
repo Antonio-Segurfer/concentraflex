259004b87b8ea8bbb7d17234ffb3fbe2624e320e
Antonio-Segurfer <127223187+Antonio-Segurfer@users.noreply.github.com>
2024-11-25 17:07:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta charset="UTF-8">
+  <title>Mailing Demo</title>
+  <link href="css\estilo.css" rel="stylesheet">
+</head>
+<body>
+<p>Hola</p>
+</body>
+</html>
+
+
+
+    
